cc2d773381ebb19403e525afb21ad63b16a96888
Kontiki94 <hassan.alheidari@hotmail.com>
2024-02-12 17:25:24 +0100

    init commit

---
+++ Silicon_AspNetMVC/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon_AspNetMVC.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+
+        ViewData["Title"] = "Silicon";
+
+        return View();
+    }
+}
