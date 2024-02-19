10807f08b6840212d7869962d05877063b7ed1fe
AnnSofie <annsofie.sjolander@gmail.com>
2024-02-19 14:52:16 +0100

    start contact

---
+++ Silicon_AspNetMVC/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon_AspNetMVC.Controllers
+{
+    public class ContactController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
