51b78f486af088ac9187b4b07be31451de2b1ce9
Kontiki94 <hassan.alheidari@hotmail.com>
2024-02-23 15:07:10 +0100

    pull from main

--- Silicon_AspNetMVC/Models/Sections/SignUpModel.cs
+++ Silicon_AspNetMVC/Models/Sections/SignUpModel.cs
@@ -6,27 +6,30 @@ namespace Silicon_AspNetMVC.Models.Sections;
 public class SignUpModel
 {
     [Display(Name = "First name", Prompt = "Enter your first name", Order = 0)]
-    [Required(ErrorMessage = "Your first name is required")]
+    [Required(ErrorMessage = "Required")]
+    [MinLength(2, ErrorMessage = "Enter a first name")]
     public string FirstName { get; set; } = null!;
 
     [Display(Name = "Last name", Prompt = "Enter your first name", Order = 1)]
-    [Required(ErrorMessage = "Your last name is required")]
+    [Required(ErrorMessage = "Required")]
+    [MinLength(2, ErrorMessage = "Enter a first name")]
     public string LastName { get; set; } = null!;
 
     [Display(Name = "Email address", Prompt = "Enter your email address", Order = 2)]
-    [Required(ErrorMessage = "Email is required")]
-    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email address")]
+    [Required(ErrorMessage = "Required")]
+    [DataType(DataType.EmailAddress)]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid")]
     public string Email { get; set; } = null!;
 
     [Display(Name = "Password", Prompt = "Enter your password", Order = 3)]
     [DataType(DataType.Password)]
-    [Required(ErrorMessage = "Invalid password")]
+    [Required(ErrorMessage = "Required")]
     [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$", ErrorMessage = "Password not strong enough")]
     public string Password { get; set; } = null!;
 
     [Display(Name = "Confirm password", Prompt = "Confirm password", Order = 4)]
     [DataType(DataType.Password)]
-    [Required(ErrorMessage = "You must confirm your password")]
+    [Required(ErrorMessage = "Confirm your password")]
     [Compare(nameof(Password), ErrorMessage = "Password does not match")]
     public string ConfirmPassword { get; set; } = null!;
 
