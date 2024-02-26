a9a4df5bcb59dd5e5f27ab74cc69d2ad7fc2e7b5
Ted Pieplow <ted.pieplow@gmail.com>
2024-02-26 15:57:17 +0100

    UpdatedOneAsync ready to go!

--- Infrastructure/Repositories/Repo.cs
+++ Infrastructure/Repositories/Repo.cs
@@ -1,7 +1,7 @@
-﻿using Azure.Identity;
-using Infrastructure.Contexts;
+﻿using Infrastructure.Contexts;
 using Infrastructure.Factories;
 using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace Infrastructure.Repositories;
@@ -10,7 +10,7 @@ public abstract class Repo<TEntity>(DataContext context) where TEntity : class
 {
     private readonly DataContext _context = context;
 
-    public virtual async Task<ResponseResult> CreateAsync(TEntity entity)
+    public virtual async Task<ResponseResult> CreateOneAsync(TEntity entity)
     {
         try
         {
@@ -27,12 +27,32 @@ public abstract class Repo<TEntity>(DataContext context) where TEntity : class
     public virtual async Task<ResponseResult> GetOneAsync(Expression<Func<TEntity, bool>> predicate)
     {
         // TODO Hassan
+        return ResponseFactory.Ok();
     }
 
     public virtual async Task<ResponseResult> GetAllAsync(TEntity entity)
     {
         // TODO Ann-Sofie
+        return ResponseFactory.Ok();
     }
 
-    
+    public virtual async Task<ResponseResult> UpdateOneAsync(Expression<Func<TEntity, bool>> predicate, TEntity updatedEntity)
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            if (result != null)
+            {
+                _context.Entry(result).CurrentValues.SetValues(updatedEntity);
+                await _context.SaveChangesAsync();
+                return ResponseFactory.Ok(result);
+            }
+
+            return ResponseFactory.NotFound(); 
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
 }
