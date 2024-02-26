393a55fc871a7fb25efb3ef016fba3d3b223e291
Ted Pieplow <ted.pieplow@gmail.com>
2024-02-26 16:43:01 +0100

    save

--- Infrastructure/Repositories/Repo.cs
+++ Infrastructure/Repositories/Repo.cs
@@ -65,10 +65,9 @@ public abstract class Repo<TEntity>(DataContext context) where TEntity : class
             {
                 _context.Set<TEntity>().Remove(result);
                 await _context.SaveChangesAsync();
-                return ResponseFactory.Ok("Successfully removed");
+                return ResponseFactory.Ok("Successfully removed.");
             }
-
-            return ResponseFactory.NotFound();
+            return ResponseFactory.NotFound(); 
         }
         catch (Exception ex)
         {
@@ -92,4 +91,8 @@ public abstract class Repo<TEntity>(DataContext context) where TEntity : class
         }
     }
 }
+
+
+
+
 
