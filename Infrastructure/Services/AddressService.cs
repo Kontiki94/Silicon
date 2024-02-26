d4e41cecdb96fe09a614672fff9ee0ddb216430e
Ted Pieplow <ted.pieplow@gmail.com>
2024-02-26 21:16:14 +0100

    AddressService in progress

---
+++ Infrastructure/Services/AddressService.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Factories;
+using Infrastructure.Models;
+using Infrastructure.Repositories;
+
+namespace Infrastructure.Services;
+
+public class AddressService(AddressRepository addressRepository)
+{
+    private readonly AddressRepository _addressRepository = addressRepository;
+
+    public async Task <ResponseResult> CreateAddressAsync()
+    {
+		try
+		{
+
+		}
+		catch (Exception ex) { return ResponseFactory.Error(ex.Message); }
+    }
+}
