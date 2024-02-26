760efce0b88b05e726bf5acada1c79bdec61247a
Ted Pieplow <ted.pieplow@gmail.com>
2024-02-26 13:20:33 +0100

    save

--- Infrastructure/Repositories/Repo.cs
+++ Infrastructure/Repositories/Repo.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Infrastructure.Contexts;
+using Infrastructure.Models;
 
-namespace Infrastructure.Repositories
+namespace Infrastructure.Repositories;
+
+public abstract class Repo<TEntity>(DataContext context) where TEntity : class
 {
-    internal class Repo
+    private readonly DataContext _context = context;
+
+    public virtual async Task<ResponseResult> CreateAsync(TEntity entity)
     {
+        try
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return new ResponseResult
+            {
+                ContentResult = entity,
+                Message = "Created successfully",
+                Status = StatusCode.OK
+            };
+        }
+        catch { }
+        return null!;
     }
 }
