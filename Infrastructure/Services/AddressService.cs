7007b852426c04306088cb1161513c9d600f58b3
Ted Pieplow <ted.pieplow@gmail.com>
2024-02-26 21:29:20 +0100

    added a addressfactory to the project

--- Infrastructure/Services/AddressService.cs
+++ Infrastructure/Services/AddressService.cs
@@ -8,12 +8,28 @@ public class AddressService(AddressRepository addressRepository)
 {
     private readonly AddressRepository _addressRepository = addressRepository;
 
-    public async Task <ResponseResult> CreateAddressAsync()
+    public async Task <ResponseResult> CreateAddressAsync(string streetName, string postalCode, string city)
     {
 		try
 		{
-
+            var exists = await _addressRepository.AlreadyExistsAsync(x => x.StreetName == streetName && x.PostalCode == postalCode && x.City == city);
+            if (exists is null)
+            {
+                var result = await _addressRepository.CreateOneAsync(AddressFactory.Create(streetName, postalCode, city));
+            }
 		}
 		catch (Exception ex) { return ResponseFactory.Error(ex.Message); }
     }
+
+    public async Task<ResponseResult> GetAddressAsync(string streetName, string postalCode, string city)
+    {
+        try
+        {
+            var result = await _addressRepository.GetOneAsync(x => x.StreetName == streetName && x.PostalCode == postalCode && x.City == city);
+            return result;
+        }
+        catch (Exception ex) { return ResponseFactory.Error(ex.Message); }
+    }
+
+
 }
