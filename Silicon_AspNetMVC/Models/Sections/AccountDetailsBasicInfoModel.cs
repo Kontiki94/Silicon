8657582a4395478e6bf38817e29dbd0fead75a8f
Ted Pieplow <ted.pieplow@gmail.com>
2024-02-26 11:43:03 +0100

    Separated Account Basic info and Address Info

--- Silicon_AspNetMVC/Models/Sections/AccountDetailsBasicInfoModel.cs
+++ Silicon_AspNetMVC/Models/Sections/AccountDetailsBasicInfoModel.cs
@@ -2,7 +2,7 @@
 
 namespace Silicon_AspNetMVC.Models.Sections;
 
-public class AccountModel
+public class AccountDetailsBasicInfoModel
 {
     [DataType(DataType.ImageUrl)]
     public string? ProfileImage { get; set; }
@@ -32,22 +32,4 @@ public class AccountModel
     [Display(Name = "Bio", Prompt = "Add a short Bio", Order = 4)]
     public string? Bio { get; set; }
 
-    [DataType(DataType.Text)]
-    [Display(Name = "Address Line 1", Prompt = "Enter your address line ", Order = 5)]
-    [Required(ErrorMessage = "Required")]
-    public string AddressLine1 { get; set; } = null!;
-
-    [DataType(DataType.Text)]
-    [Display(Name = "Address Line 2", Prompt = "Enter your second address line ", Order = 6)]
-    public string? AddressLine2 { get; set; }
-
-    [DataType(DataType.Text)]
-    [Required(ErrorMessage = "Required")]
-    [Display(Name = "City", Prompt = "Enter your city", Order = 7)]
-    public string City { get; set; } = null!;
-
-    [DataType(DataType.Text)]
-    [Required(ErrorMessage = "Required")]
-    [Display(Name = "Postal Code", Prompt = "Postal code", Order = 8)]
-    public string PostalCode { get; set; } = null!;
 }
---
+++ Silicon_AspNetMVC/ViewModels/CompositeViewModels/AccountViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Silicon_AspNetMVC.Models.Sections;
+namespace Silicon_AspNetMVC.ViewModels.CompositeViewModels;
+
+public class AccountViewModel
+{
+    public AccountDetailsBasicInfoModel Details {  get; set; }
+    public AccountDetailsAddressInfoModel AddressInfo { get; set; }
+
+    public AccountViewModel()
+    {
+        var basicInfoViewModel = new AccountDetailsBasicInfoViewModel();
+        Details = basicInfoViewModel.Account;
+
+        var addressInfoViewModel = new AccountDetailsAddressInfoViewModel();
+        AddressInfo = addressInfoViewModel.Address;
+    }
+}
