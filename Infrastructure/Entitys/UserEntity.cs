964d1fe512104065326dc8e6d90e388df3f8e0d7
Ted Pieplow <ted.pieplow@gmail.com>
2024-02-26 10:14:38 +0100

    added UserEntity

--- Infrastructure/Entitys/UserEntity.cs
+++ Infrastructure/Entitys/UserEntity.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace Infrastructure.Entitys
+namespace Infrastructure.Entitys;
+
+public class UserEntity
 {
-    internal class UserEntity
-    {
-    }
+    [Key]
+    public string Id { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string SecurityKey { get; set; } = null!;
+    public string? Phone {  get; set; } = null!;
+    public string? Biography { get; set; }
+    public DateTime? Created { get; set; }
+    public DateTime? Updated { get; set;}
 }
--- Silicon_AspNetMVC/Models/Sections/AccountModel.cs
+++ Silicon_AspNetMVC/Models/Sections/AccountModel.cs
@@ -7,10 +7,12 @@ public class AccountModel
     [DataType(DataType.ImageUrl)]
     public string? ProfileImage { get; set; }
 
+    [DataType(DataType.Text)]
     [Display(Name = "First name", Prompt = "John", Order = 0)]
     [Required(ErrorMessage = "Required")]
     public string FirstName { get; set; } = null!;
 
+    [DataType(DataType.Text)]
     [Display(Name = "Last name", Prompt = "Doe", Order = 1)]
     [Required(ErrorMessage = "Required")]
     public string LastName { get; set; } = null!;
@@ -21,24 +23,30 @@ public class AccountModel
     [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email address")]
     public string Email { get; set; } = null!;
 
+    [DataType(DataType.PhoneNumber)]
     [Display(Name = "Phone", Prompt = "Phone number", Order = 3)]
     [Required(ErrorMessage = "Required")]
     public string Phone { get; set; } = null!;
 
+    [DataType(DataType.Text)]
     [Display(Name = "Bio", Prompt = "Add a short Bio", Order = 4)]
     public string? Bio { get; set; }
 
+    [DataType(DataType.Text)]
     [Display(Name = "Address Line 1", Prompt = "Enter your address line ", Order = 5)]
     [Required(ErrorMessage = "Required")]
     public string AddressLine1 { get; set; } = null!;
 
+    [DataType(DataType.Text)]
     [Display(Name = "Address Line 2", Prompt = "Enter your second address line ", Order = 6)]
     public string? AddressLine2 { get; set; }
 
+    [DataType(DataType.Text)]
     [Required(ErrorMessage = "Required")]
     [Display(Name = "City", Prompt = "Enter your city", Order = 7)]
     public string City { get; set; } = null!;
 
+    [DataType(DataType.Text)]
     [Required(ErrorMessage = "Required")]
     [Display(Name = "Postal Code", Prompt = "Postal code", Order = 8)]
     public string PostalCode { get; set; } = null!;
