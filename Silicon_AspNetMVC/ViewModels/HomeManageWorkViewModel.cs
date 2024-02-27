bfe6a1a3aebc92f16082d305ae47913de01a75d2
Ted Pieplow <ted.pieplow@gmail.com>
2024-02-27 09:06:10 +0100

    Made a ViewModel for ManageWork

---
+++ Silicon_AspNetMVC/ViewModels/HomeManageWorkViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Silicon_AspNetMVC.ViewModels
+{
+    public class HomeManageWorkViewModel
+    {
+        public List<string>? Features { get; set; }
+        public string ButtonText { get; set; }
+        public string ButtonIcon { get; set; }
+
+        public HomeManageWorkViewModel() 
+        {
+            Features = new List<string>()
+            {
+                "Powerful project management",
+                "Transparent work management",
+                "Manage work & focus on the most important tasks",
+                "Track your progress with interactive charts",
+                "Easiest way to track time spent on tasks"
+            };
+
+            ButtonText = "Learn more";
+            ButtonIcon = "fa-solid fa-arrow-right";
+        }
+    }
+}
