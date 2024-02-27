2dd15dec2580326b6e3eb22f94a7ea617d8dec79
Ted Pieplow <ted.pieplow@gmail.com>
2024-02-27 17:19:58 +0100

    added comments to PasswordHasher

--- Infrastructure/Helpers/PasswordHasher.cs
+++ Infrastructure/Helpers/PasswordHasher.cs
@@ -7,9 +7,17 @@ namespace Infrastructure.Helpers
 {
     public class PasswordHasher
     {
+        // The size of the salt.
         private const int SaltSize = 32;
+        // Private key used in the HMAC-algorithm to create a hashed password otogether with the salt.
         private const string SecurityKey = "16";
 
+        /// <summary>
+        /// Creates a instance of HMACSHA3_512 with the securitykey, then the salt is introduced to the HMAC-object.
+        /// Converts the password to bytes and the HMACSHA3_512 algo is used to calculate the hashvalue.
+        /// </summary>
+        /// <param name="password">The input password from the user</param>
+        /// <returns>Returns salt and hash as 64base-coded strings</returns>
         public static (string, string) GenerateSecurePassword(string password)
         {
             byte[] salt = GenerateSalt();
@@ -22,6 +30,17 @@ namespace Infrastructure.Helpers
             return (Convert.ToBase64String(salt), Convert.ToBase64String(hashedPassword));
         }
 
+        /// <summary>
+        /// Validates a password against a already generated hashvalue
+        /// Hash and salt converts to byte-arrays
+        /// Creates a instance of the HMAC-algo with the SecurityKey as a key and the salt is assigned the HMAC-object.
+        /// The password is converted to bytes and HMAC calculate the hash value.
+        /// Lastely, the two hashvalues are compared against each other
+        /// </summary>
+        /// <param name="password">The input password from the user</param>
+        /// <param name="hash">The calulated hash</param>
+        /// <param name="salt">Salting</param>
+        /// <returns>True if valid, else false</returns>
         public static bool ValidateSecurePassword(string password, string hash, string salt)
         {
             byte[] saltBytes = Convert.FromBase64String(salt);
@@ -34,6 +53,10 @@ namespace Infrastructure.Helpers
             return AreHashesEqual(hashBytes, computedHash);
         }
 
+        /// <summary>
+        /// Generates a random salt based on the SaltSize.
+        /// </summary>
+        /// <returns></returns>
         private static byte[] GenerateSalt()
         {
             byte[] salt = new byte[SaltSize];
@@ -44,6 +67,13 @@ namespace Infrastructure.Helpers
             return salt;
         }
 
+        /// <summary>
+        /// Compares two hashvalues against each other. 
+        /// First we controll the length of the two byte-arrays against each other, secondly we control the byte separately
+        /// </summary>
+        /// <param name="hash1">Represents the saved hash-value</param>
+        /// <param name="hash2">Represents the new (input) hash-value</param>
+        /// <returns></returns>
         private static bool AreHashesEqual(byte[] hash1, byte[] hash2)
         {
             if (hash1.Length != hash2.Length)
