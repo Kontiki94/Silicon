306a149f4d0a152df47d33eaf111b90406ac8973
Ted Pieplow <ted.pieplow@gmail.com>
2024-03-01 14:17:19 +0100

    save

--- Silicon_AspNetMVC/Controllers/AccountController.cs
+++ Silicon_AspNetMVC/Controllers/AccountController.cs
@@ -1,86 +1,64 @@
-﻿using Infrastructure.Factories;
-using Infrastructure.Services;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Silicon_AspNetMVC.ViewModels;
 using Silicon_AspNetMVC.ViewModels.Account;
 using Silicon_AspNetMVC.ViewModels.CompositeViewModels;
 
-namespace Silicon_AspNetMVC.Controllers;
-
-public class AccountController(UserService userService) : Controller
+namespace Silicon_AspNetMVC.Controllers
 {
-    private readonly UserService _userService = userService;
-
-    [Route("/details")]
-    [HttpGet]
-    public IActionResult Details()
+    public class AccountController : Controller
     {
-        var viewModel = new AccountViewModel();
-        ViewData["Title"] = "Details";
-        return View(viewModel);
-    }
-
-    [Route("/details")]
-    [HttpPost]
-    public IActionResult Details(AccountViewModel viewmodel)
-    {
-        if (!ModelState.IsValid)
+        [Route("/details")]
+        [HttpGet]
+        public IActionResult Details()
         {
-            return View(viewmodel);
+            var viewModel = new AccountViewModel();
+            ViewData["Title"] = "Details";
+            return View(viewModel);
         }
 
-        return RedirectToAction(nameof(Details), viewmodel);
-    }
-
-    [Route("/security")]
-    [HttpGet]
-    public IActionResult Security()
-    {
-        var viewModel = new AccountSecurityViewModel();
-        ViewData["Title"] = "Security";
-        return View(viewModel);
-    }
-
-    [Route("/security")]
-    [HttpPost]
-    public IActionResult Security(AccountSecurityViewModel viewModel)
-    {
-        if (ModelState.IsValid)
+        [Route("/details")]
+        [HttpPost]
+        public IActionResult Details(AccountViewModel viewmodel)
         {
-            var result = _userService.UpdateCredentials(viewModel.Security);
-            if (result.Result.StatusCode == Infrastructure.Models.StatusCode.OK)
+            if (!ModelState.IsValid)
             {
-                return RedirectToAction("Details", "Account");
+                return View(viewmodel);
             }
-        }
-        return View(viewModel);
-    }
 
-    [Route("/security")]
-    [HttpDelete]
-    public IActionResult Delete()
-    {
-        if (ModelState.IsValid)
+            return RedirectToAction(nameof(Details), viewmodel);
+        }
+
+        [Route("/security")]
+        [HttpGet]
+        public IActionResult Security()
         {
-            var result = _userService.DeleteUser();
-            if(result.Result.StatusCode == Infrastructure.Models.StatusCode.OK)
-            {
-                return RedirectToAction("SignIn", "Auth");
-            }
+            var viewModel = new AccountSecurityViewModel();
+            ViewData["Title"] = "Security";
+            return View(viewModel);
         }
-        return View();
-    }
 
-    public IActionResult Cancel()
-    {
-        return RedirectToAction("Details", "Account");
-    }
+        [Route("/security")]
+        [HttpPost]
+        public IActionResult Security(AccountSecurityViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            return RedirectToAction("Details", "Account");
+        }
 
-    [Route("/account/saved")]
-    public IActionResult SavedCourses()
-    {
-        var viewModel = new SavedCoursesViewModel();
-        return View(viewModel);
-    }
+        public IActionResult Cancel()
+        {
+            return RedirectToAction("Details", "Account");
+        }
 
+        [Route("/account/saved")]
+        public IActionResult SavedCourses()
+        {
+            var viewModel = new SavedCoursesViewModel();
+            return View(viewModel);
+        }
+
+    }
 }
