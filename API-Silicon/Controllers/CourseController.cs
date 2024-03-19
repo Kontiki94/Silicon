fe19b657f29eb4d486f141f79967a56109de65d1
Ted Pieplow <ted.pieplow@gmail.com>
2024-03-19 16:08:40 +0100

    save

---
+++ API-Silicon/Controllers/CourseController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Silicon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+
+        #region READ
+        [HttpGet]
+        public IActionResult GetOne()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+        #endregion
+
+        #region UPDATE
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+        #endregion
+
+        #region DELETE
+        [HttpPut]
+        public IActionResult Delete()
+        {
+            return Ok();
+
+        }
+        #endregion
+
+    }
+}
