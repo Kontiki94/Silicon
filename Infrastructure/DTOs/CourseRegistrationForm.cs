ad689c854250ecb6917a954be47427bf644a26df
Ted Pieplow <ted.pieplow@gmail.com>
2024-03-20 22:05:23 +0100

    Create course works fine aswell

--- Infrastructure/DTOs/CourseRegistrationForm.cs
+++ Infrastructure/DTOs/CourseRegistrationForm.cs
@@ -6,14 +6,24 @@ namespace Infrastructure.DTOs;
 public class CourseRegistrationForm
 {
     [Required]
+    [Display(Name = "Title")]
     public string Title { get; set; } = null!;
+    [Display(Name = "Price")]
     public string? Price { get; set; }
+    [Display(Name = "Discount Price")]
     public string? DiscountPrice { get; set; }
+    [Display(Name = "Course Image")]
     public string? CourseImage { get; set; }
+    [Display(Name = "Rating")]
     public string? Rating { get; set; }
+    [Display(Name = "Reviews")]
     public string? Reviews { get; set; }
+    [Display(Name = "Views")]
     public string? Views { get; set; }
+    [Display(Name = "Likes")]
     public string? Likes { get; set; }
+    [Display(Name = "View hours")]
     public string? ViewHours { get; set; }
+    [Display(Name = "Author Name")]
     public string? AuthorName { get; set; }
 }
