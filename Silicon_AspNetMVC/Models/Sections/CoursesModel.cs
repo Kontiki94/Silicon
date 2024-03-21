b9997c6032425bfd0e7a4d52cf7713b25d5acbdd
Ted Pieplow <ted.pieplow@gmail.com>
2024-03-21 10:46:11 +0100

    save before merge

--- Silicon_AspNetMVC/Models/Sections/CoursesModel.cs
+++ Silicon_AspNetMVC/Models/Sections/CoursesModel.cs
@@ -4,14 +4,33 @@
     {
         public int Id { get; set; }
         public string Title { get; set; } = null!;
+        public bool IsBestSeller { get; set; } = false;
+        public string? CourseImage { get; set; }
         public string? Price { get; set; }
         public string? DiscountPrice { get; set; }
-        public string? CourseImage { get; set; }
         public string? Rating { get; set; }
         public string? Reviews { get; set; }
         public string? Views { get; set; }
-        public string? Likes { get; set; }
-        public string? ViewHours { get; set; }
+        public string? LikesInPercent { get; set; }
+        public string? LikesInNumbers { get; set; }
+
+
+        // AutherInfo
         public string? AuthorName { get; set; }
+        public string? AutherBio { get; set; }
+        public string? AuthorImage { get; set; }
+        public string? YouTubeSubscribers { get; set; }
+        public string? FaceBookFollowers { get; set; }
+
+
+        // CourseDetails
+        public string? CourseDescription { get; set; }
+        public string? ViewHours { get; set; }
+        public string? Articles { get; set; }
+        public string? Resources { get; set; }
+        public string? AccessTime { get; set; }
+        public List<string>? ProgramDetails { get; set; }
+        public List<string>? LearnPoints { get; set; }
+        public List<string>? Categories { get; set; }
     }
 }
