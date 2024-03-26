793ca1a4e9ec4412117187a13c1e876e59d4ff85
Kontiki94 <hassan.alheidari@hotmail.com>
2024-03-26 01:04:04 +0100

    mega mergeconflict resolved

--- Silicon_AspNetMVC/ViewModels/Courses/CoursesViewModel.cs
+++ Silicon_AspNetMVC/ViewModels/Courses/CoursesViewModel.cs
@@ -6,13 +6,8 @@ namespace Silicon_AspNetMVC.ViewModels.Courses
     {
         public string Title { get; set; } = null!;
         public IEnumerable<CoursesModel> AllCourses { get; set; } = [];
-<<<<<<< HEAD
-
         public IEnumerable<CourseDetailsModel> Courses { get; set; } = [];
         public string? ErrorMessage { get; set; }
-        
-=======
         public CoursesModel OneCourse { get; set; } = new CoursesModel();
->>>>>>> hassans_branch
     }
 }
