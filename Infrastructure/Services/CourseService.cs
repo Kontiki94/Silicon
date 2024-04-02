77f8e6123038a45e3087ef2edd9e1780658f5d33
Ted Pieplow <ted.pieplow@gmail.com>
2024-04-02 22:55:23 +0200

    Added filter for dropdown and enabled search query

---
+++ Infrastructure/Services/CourseService.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace Infrastructure.Services;
+
+public class CourseService(HttpClient http, IConfiguration configuration)
+{
+    private readonly HttpClient _http = http;
+    private readonly IConfiguration _configuration = configuration;
+
+    public async Task<IEnumerable<CoursesModel>> GetCoursesAsync(string category = "", string searchQuery = "")
+    {
+        var apiResponse = await _http.GetAsync($"{_configuration["ApiUris:Courses"]}?category={Uri.UnescapeDataString(category)}&searchQuery={Uri.UnescapeDataString(searchQuery)}&key={_configuration["ApiKey:Secret"]}");
+        if (apiResponse.IsSuccessStatusCode)
+        {
+            var json = await apiResponse.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<CourseResult>(json);
+            if (result!.Courses != null && result.Succeeded)
+                return result.Courses;
+        }
+
+        return null!;
+    }
+}
--- Silicon_AspNetMVC/ViewModels/Courses/CoursesViewModel.cs
+++ Silicon_AspNetMVC/ViewModels/Courses/CoursesViewModel.cs
@@ -1,11 +1,12 @@
-﻿using Silicon_AspNetMVC.Models.Sections;
+﻿using Infrastructure.Models;
 
 namespace Silicon_AspNetMVC.ViewModels.Courses
 {
     public class CoursesViewModel
     {
-        public string Title { get; set; } = null!;
-        public IEnumerable<CoursesModel> AllCourses { get; set; } = [];
+        //public string Title { get; set; } = null!;
+        public IEnumerable<CategoryModel>? Categories { get; set; }
+        public IEnumerable<CoursesModel>? AllCourses { get; set; }
         public CoursesModel OneCourse { get; set; } = new CoursesModel();
         public string? ErrorMessage { get; set; }
         
